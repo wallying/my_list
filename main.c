42d030ea68edc36ee50640adebb26129e0d60dd2
wallying <wallying@foxmail.com>
2019-12-17 10:19:11 +0800

    update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /**
  * file:    main.c
  * author:  wallying@foxmail.com
- * date:    2019-12-16
+ * date:    2019-12-17
  **/
 
 
@@ -14,21 +14,21 @@ void print_node(void *node);
 
 
 /*******************************************************************************
-list单向链表使用方法：
-1.定义用户节点结构体；
-    例如： struct item_t {...};
-    1).节点结构体的第一项必须为本结构体的指针变量，即struct item_t *next;
-    2).节点结构体的其他项可以为节点所携带的信息变量，例如void *data;
-2.定义用户节点变量：
-    例如：struct item_t item;
-3.定义用户链表变量；
+lib_list单向链表使用方法:
+1.定义链表变量(类型必须为list_t)
     例如：list_t link_list;
-    1).链表的类型必须为list_t;
-4.使用链表。
-    1).所有的操作，均需要使用链表的地址、节点的地址作为函数形参；
-        例如向链表link_list的表头插入item节点：
-        list_insert_head(&link_list, &item)
+2.定义节点结构体(结构体首项必须为本结构体指针)
+    结构体余项可以为数据信息携带指针,如void *data;
+    例如：struct item_t {struct item_t *next; void *data; ...};
+3.定义节点变量
+    例如：struct item_t item;
+4.使用链表(函数形参必须为链表地址、节点地址)
+    list_init(&link_list);
+    list_insert_head(&link_list, &item)
+    list_insert_tail(&link_list, &item);
 *******************************************************************************/
+
+list_t link_list = NULL;
 
 typedef struct _item_t {
     struct _item_t *next;
@@ -42,7 +42,6 @@ myItem_t item4 = {NULL, "04"};
 myItem_t item5 = {NULL, "05"};
 myItem_t item6 = {NULL, "06"};
 
-list_t link_list = NULL;
 
 
 int main(int argc, char *argv[])
@@ -67,8 +66,6 @@ int main(int argc, char *argv[])
 
     list_init(&link_list);print_list();
     print_node(list_tail(&link_list));printf("\r\n");
-
-
 
 
 
